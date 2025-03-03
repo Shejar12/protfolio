b0d2428605e6744f33e8bfa20823398f7ce27f4f
Sejar <sejarhang12@gmail.com>
2025-03-03 13:24:43 +0545

    first commit

---
+++ src/components/About.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import aboutImg from '../assets/about.jpg'
+
+const About = () => {
+  return (
+    <div className='text-white max-w-[1400px] mx-auto my-12' id="about">
+      <div className='md:grid md:grid-cols-2 sm:py-16'>
+        <div className='mt-4 md:mt-0 text-left flex '>
+          <div className='my-auto mx-6'>
+            {/* header section */}
+            <h2 className='text-4xl font-bold mb-4 primary-color'>
+              About Me
+            </h2>
+            {/* paragraph section about me */}
+            <p className='text-base lg:text-lg'>
+            I am a web developer and UI/UX designer with expertise in HTML, CSS, JavaScript, React, WordPress, Bootstrap, and Tailwind CSS. I specialize in creating responsive, user-friendly, and visually appealing websites that enhance user experience and meet client requirements. My strong grasp of modern design principles and front-end technologies allows me to develop intuitive and functional digital solutions. Passionate about crafting seamless user experiences, I am committed to delivering high-quality designs and development that align with industry standards.
+            </p>
+          </div>
+
+        </div>
+        {/* Image section for about me*/}
+        <img className='mx-auto rounded-3xl py-8 md:py-0' src={aboutImg} width={300} height={300} />
+      </div>
+    </div>
+  )
+}
+
+export default About
---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className='max-w-[1400px] sm:h-[150px] p-12 flex justify-between mx-auto'>
+      <span className='primary-color'>Shejar Kandangwa</span>
+      <p className='text-gray-600'>sejarhang12@gmail.com</p>
+    </div>
+  )
+}
+
+export default Footer
