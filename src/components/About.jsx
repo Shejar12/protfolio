3bd38dd932d234473b4c2ca5d9a85382955217eb
Shejar Kandangwa <125634374+Shejar12@users.noreply.github.com>
2025-03-16 09:32:29 +0545

    Update About.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -13,7 +13,7 @@ const About = () => {
             </h2>
             {/* paragraph section about me */}
             <p className='text-base lg:text-lg'>
-            I am a web developer and UI/UX designer with expertise in HTML, CSS, JavaScript, React, WordPress, Bootstrap, and Tailwind CSS. I specialize in creating responsive, user-friendly, and visually appealing websites that enhance user experience and meet client requirements. My strong grasp of modern design principles and front-end technologies allows me to develop intuitive and functional digital solutions. Passionate about crafting seamless user experiences, I am committed to delivering high-quality designs and development that align with industry standards.
+            I am a web developer and UI/UX designer with expertise in HTML, CSS, JavaScript, React, WordPress, Bootstrap, Tailwind CSS,Node.js. I specialize in creating responsive, user-friendly, and visually appealing websites that enhance user experience and meet client requirements. My strong grasp of modern design principles and front-end technologies allows me to develop intuitive and functional digital solutions. Passionate about crafting seamless user experiences, I am committed to delivering high-quality designs and development that align with industry standards.
             </p>
           </div>
 
